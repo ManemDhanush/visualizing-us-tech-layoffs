4cec3c146b5242d77e5589b8346c4fe6161d9bac
Nitin Gopala Krishna Sontineni <f20180262@hyderabad.bits-pilani.ac.in>
2024-05-08 18:49:55 -0400

    Add files via upload

---
+++ src/charts/ChoroplethMap.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const ChoroplethMap = ( {props, handleStateChange} ) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const usStates = props.choroplethMap;
+    // console.log(usStates);
+    const svg = d3.select(ref.current);
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+
+    const projection = d3.geoAlbersUsa().fitSize([width, height], usStates);
+
+    const colorScale = d3
+      .scaleLog()
+      .domain([1, d3.max(usStates.features, d => d.value)])
+      .range(['#ff9999','#ff0000','#800000']);
+
+    const path = d3.geoPath().projection(projection);
+
+    const handleClick = (event, d) => {
+      // console.log(event);
+
+      const statePath = svg.select(`#${event.srcElement.id.replace(/\s+/g, '')}`);
+      const fillColor = statePath.attr('fill');
+      const newFillColor = fillColor === '#8bc34a' ? colorScale(event.value) : '#8bc34a';
+      statePath.attr('fill', newFillColor);
+
+      if (newFillColor === '#8bc34a') {
+        handleStateChange(event.srcElement.id); // call handleStateChange method here
+      } else {
+        handleStateChange("US");
+      }
+    };
+
+    svg
+      .selectAll('path')
+      .data(usStates.features)
+      .join('path')
+      .attr('id', d => d.properties.name.replace(/\s+/g, ''))
+      .attr('d', path)
+      .attr('fill', d => colorScale(d.value))
+      .on('click', handleClick)
+      .append('title')
+      .text(d => d.properties.name + " " + d.value);
+  }, [props.start, props.end]);
+
+  return (
+    <svg ref={ref} width={500} height={280}>
+    </svg>
+  );
+};
+
+export default ChoroplethMap;
